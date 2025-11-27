5ad87270836728b06b516d2fbf4dcf03a8a560f1
PaulaSuanoMoron <smpaula916@gmail.com>
2025-11-27 19:57:26 +0100

    limpio

--- clases/producto.java
+++ clases/producto.java
@@ -7,7 +7,6 @@ public abstract class producto
 private String nombre;
 private String descripcion;
 private int numSerie;
-private int stock;
 private double precioInicial;
 
 // CONSTRUCTOR: para inicializar las variables que han sido creadas anteriormente
@@ -16,7 +15,6 @@ public producto (String nombre, String descripcion, int numSerie, int stock, dou
 this.nombre= nombre;
 this.descripcion= descripcion;
 this.numSerie= numSerie;
-this.stock= stock;
 this.precioInicial= precioInicial;
 }
 
@@ -51,26 +49,5 @@ public abstract double calcularElPrecioFinal();
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*✅ Bien (Abstracto):
-
-Java
-
-public abstract double calcularPrecioFinal();
-2. Obliga a las Subclases a Implementarlo
-Aquí es donde está la clave de la palabra "OBLIGA":
-
-Cuando defines un método como abstract en la clase base (Producto), estás diciendo: "Cualquier clase que herede de mí debe proporcionar su propia versión de este método."
-
-Si una subclase (como ProductoFisico o ProductoDigital) no implementa (@Override) ese método abstracto, el compilador de Java te dará un error y no te dejará ejecutar el programa. */
 
 }
